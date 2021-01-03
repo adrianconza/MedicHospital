73d3be1d4b33667258beca709dfd926e45577dc4
Adrian <hola@adrianconza.com>
2021-01-03 16:22:42 -0500

    Added Models for cities and provinces

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
 use App\Models\ImagingExam;
 use App\Models\LaboratoryExam;
 use App\Models\MedicalSpeciality;
+use App\Models\Province;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,8 +19,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-         MedicalSpeciality::factory(50)->create();
-         LaboratoryExam::factory(50)->create();
-         ImagingExam::factory(50)->create();
+        MedicalSpeciality::factory(50)->create();
+        LaboratoryExam::factory(50)->create();
+        ImagingExam::factory(50)->create();
+        Province::factory(10)->create()->each(function ($province) {
+            $province->cities()->saveMany(City::factory(5)->make());
+        });
     }
 }
